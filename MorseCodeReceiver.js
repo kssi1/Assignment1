0ed3f0b31c9e0acb450359e139fb6cd76866da3c
tsit9 <tsit9@student.monash.edu>
2016-11-17 02:41:14 +1100

    Update MorseCodeReceiver.js

--- MorseCodeReceiver.js
+++ MorseCodeReceiver.js
@@ -37,11 +37,73 @@ var last,chara,word;
  * Output: You should return a boolean denoting whether or not the image is
  *		 an 'on' (blue) signal.
  */
+
+// Curate: The function Curate uses .length to determinate the length of the array, and processes the array from the first value. The array taken would be a multiple of four array.
+// For every three values in the array, the function will splice the forth one, which would be the Alpha, which does not have any impact on colour.
+// It then returns the spliced array, which should be a multiple of three.
+
+
+// Count: Count initially takes the input of the multiple of three array, then looks at a "set" (three arrays representing Red, Green and Blue)
+// The highest of the set then adds one to the RGB counter. The function then looks at the next set of arrays, and repeats.
+// At the end, the function looks at the highest count, then returns FALSE if red was highest, TRUE if blue was highest or ERROR if green was highest.
+function curate(array)
+{
+	var length = array.length
+	var n = 0
+	var arrayPositionSplice = 0
+	for(var i=0; i<length; i++)
+	{
+		if(n===3)
+		{
+			array.splice(i-arrayPositionSplice,1) //-arrayPositionSplice is done because after splicing an array, the ones afterwards gets shifted down an index.
+			n=0
+			arrayPositionSplice++
+			
+		}
+		else
+		{
+			n=n+1
+			
+		}
+		
+	}
+	return array
+	
+}
+
+function count(input)
+{
+	var length = input.length
+	var arraySets = length/3
+	var red = 0
+	var green = 0
+	var blue = 0
+	var redCount = 0
+	var greenCount = 0
+	var blueCount = 0
+	var n = 0
+	for(var i = 0;i < arraySets;i++)
+	{
+		red=input[n]
+		green=input[n+1]
+		blue=input[n+2]
+		if ((red > green) && (red > blue)){redCount++}
+		else if ((green > red) && (green > blue)){greenCount++}
+		else if ((blue > green) && (blue > red)){blueCount++}
+		n=n+3
+	}
+	if ((redCount > greenCount) && (redCount > blueCount)){return false}
+	else if ((greenCount > redCount) && (greenCount > blueCount)){return "error"}  //may need to ignore the condition, and simply return as error in case of white paper,etc.
+	else if ((blueCount > greenCount) && (blueCount > redCount)){return true}
+}
+
+
 function decodeCameraImage(data)
 {
-	// ADD YOUR CODE HERE
+	var trimmedArray=curate(data)
+	
 
-	return false;
+	return count(trimmedArray);
 }
 
 function signalConverting(data)
